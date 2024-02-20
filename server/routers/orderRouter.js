f30948d02fbcb1c5e1e48d8019ad441666de4b65
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2024-02-20 22:49:16 +0530

    order routes

--- server/routers/orderRouter.js
+++ server/routers/orderRouter.js
@@ -1,9 +1,17 @@
 const express=require("express");
 const router=express.Router();
 
-const { authenticateToken }=require("../middlewares/authMiddleware");
+const { authenticateToken, authorizeRoles }=require("../middlewares/authMiddleware");
 const orderController=require("../controllers/orderController");
 
+router.get('/',authenticateToken,authorizeRoles(['admin']),orderController.getAllOrders);
 router.post('/',authenticateToken,orderController.createOrder);
+
+router.get("/placed-orders",authenticateToken,orderController.getPlacedOrders);
+router.get("/deliever-orders",authenticateToken,orderController.getDeliveredOrders);
+router.get("/cancelled-orders",authenticateToken,orderController.getCancelledOrders);
+
+router.post('/cancel-order',authenticateToken,orderController.cancelOrder);
+router.delete('/:id',authenticateToken,authorizeRoles(['admin']),orderController.deleteOrder);
 
 module.exports=router;
