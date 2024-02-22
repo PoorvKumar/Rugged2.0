2d2a9e668f62aed29407b0a379ef909c8a860292
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2024-02-22 22:52:07 +0530

    multer

--- server/app.js
+++ server/app.js
@@ -5,11 +5,13 @@ const cors=require("cors");
 const corsOptions=require("./config/corsOptions");
 const connectDB=require("./config/db");
 const morgan=require("morgan");
+const multer=require("multer");
 const rfs=require("rotating-file-stream");
 const path = require("path");
 const app=express();
 
 const errorMiddleware=require("./middlewares/errorMiddleware");
+const notFoundMiddleware=require("./middlewares/notFoundMIddleware");
 
 const authRouter=require("./routers/authRouter");
 const userRouter=require("./routers/userRouter");
@@ -17,7 +19,8 @@ const orderRouter=require("./routers/orderRouter");
 const blogRouter=require("./routers/blogRouter");
 const sellerRouter=require("./routers/sellerRouter");
 const productRouter = require("./routers/productRoutes");
-const cartRouter=require("./routers/cartRouter")
+const cartRouter=require("./routers/cartRouter");
+const { authenticateToken } = require("./middlewares/authMiddleware");
 
 //Database Connection
 connectDB();
@@ -36,22 +39,41 @@ const accessStream=rfs.createStream('access.log',{
 
 app.use(morgan("combined",{ stream: accessStream }));
 
+// Multer storage configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/') // specify the folder where files will be stored
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname) // specify the file naming convention
+  }
+});
+
+const upload = multer({ storage: storage });
+
 app.use('/api/auth',authRouter);
 app.use('/api/users',userRouter);
 app.use('/api/orders',orderRouter);
 app.use('/api/blogs',blogRouter);
 app.use('/api/seller',sellerRouter);
 app.use('/api/products',productRouter);
-app.use('/api/cart',cartRouter)
+app.use('/api/cart',cartRouter);
+
+app.post('/api/uploads',authenticateToken, upload.array('files', 5), (req, res) => {
+  if (!req.files || req.files.length === 0) {
+    return res.status(400).json({ message: 'No files uploaded' });
+  }
+
+  const imageUrls = req.files.map(file => `${req.protocol}://${req.get('host')}/uploads/${file.filename}`);
+  res.status(200).json({ imageUrls: imageUrls });
+});
+
 app.get("/", (req, res) => {
   return res.json({ msg: "Server running!" });
 });
 
 // Route not found
-app.use((req,res,next)=>
-{
-  return res.status(404).json({ msg: "Route not found" });
-})
+app.use(notFoundMiddleware);
 
 app.use(errorMiddleware);
 
