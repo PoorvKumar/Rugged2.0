a44694476a2ce004b1908d23ce884d4d223810ca
Akash <akashkulkarni4444@gmail.com>
2024-04-30 19:59:24 +0530

    Correct redis implementation of search with pagination done.

--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -59,127 +59,205 @@ const getAverageRating = (product) => {
   return avg;
 };
 
+// const getSearchedProducts = async (req, res, next) => {
+//   try {
+//     const searchTerm = req.query.q;
+//     let pattern = new RegExp(searchTerm, "i");
+//     let products = await Product.find({
+//       $or: [
+//         { name: { $regex: searchTerm, $options: "i" } },
+//         { categories: { $in: [pattern] } },
+//         { tags: { $in: [pattern] } },
+//         { brand: { $in: [pattern] } },
+//       ],
+//     });
+
+//     const customerRating = Number(req.query.customerRating);
+//     const brands = req.query.brands.split(",");
+//     const priceLL = Number(req.query.priceLL);
+//     const priceUL = Number(req.query.priceUL);
+//     const ruggedVerified = req.query.RuggedVerrified;
+//     const coloursSelected = req.query.colours.split(",");
+//     const availability = req.query.availability;
+//     const pageNo = req.query.pageNo;
+//     const noOfResultsPerPage = Number(req.query.noOfResultsPerPage);
+//     const categoriesSelected = req.query.categories.split(",");
+
+//     const priceFilter = (product) => {
+//       let priceAfterDiscount = product.price * (1 - product.discount * 0.01);
+//       return priceAfterDiscount > priceLL && priceAfterDiscount < priceUL;
+//     };
+
+//     const customerRatingFilter = (product) => {
+//       // console.log(getAverageRating(product));
+//       return getAverageRating(product) >= customerRating;
+//     };
+
+//     const brandFilter = (product) => {
+//       let index = 0;
+//       for (index = 0; index < brands.length; index++) {
+//         if (product.brand === brands[index]) {
+//           return true;
+//         }
+//       }
+//       return false;
+//     };
+
+//     const ruggedVerrifiedFilter = (product) => {
+//       return product.ruggedVerrified === true;
+//     };
+
+//     const colorFilter = (product) => {
+//       let index = 0;
+//       for (index = 0; index < coloursSelected.length; index++) {
+//         let jindex = 0;
+//         for (jindex = 0; jindex < product.colours.length; jindex++) {
+//           if (coloursSelected[index] === product.colours[jindex]) {
+//             return true;
+//           }
+//         }
+//       }
+//       return false;
+//     };
+
+//     const categoriesFilter = (product) => {
+//       let index = 0;
+//       for (index = 0; index < categoriesSelected.length; index++) {
+//         let jindex = 0;
+//         for (jindex = 0; jindex < product.categories.length; jindex++) {
+//           if (categoriesSelected[index] === product.categories[jindex]) {
+//             return true;
+//           }
+//         }
+//       }
+//       return false;
+//     };
+//     const availabilityFilter = (product) => {
+//       return product.stockQuantity >= 1;
+//     };
+//     // Filter the products based on user's requirements and sort them accordingly
+//     if (brands[0] !== "all") {
+//       products = products.filter(brandFilter);
+//     }
+//     if (ruggedVerified === "true") {
+//       products = products.filter(ruggedVerrifiedFilter);
+//     }
+
+//     if (coloursSelected[0] !== "all") {
+//       products = products.filter(colorFilter);
+//     }
+//     if (categoriesSelected[0] !== "all") {
+//       products = products.filter(categoriesFilter);
+//     }
+
+//     if (availability) {
+//       products = products.filter(availabilityFilter);
+//     }
+
+//     if (true) {
+//       products = products.filter(priceFilter);
+//     }
+
+//     if (customerRating > 0) {
+//       products = products.filter(customerRatingFilter);
+//     }
+
+//     let newProducts = [];
+//     let i = 0;
+//     let j = 0;
+//     let ilimit =
+//       noOfResultsPerPage * pageNo < products.length
+//         ? noOfResultsPerPage * pageNo
+//         : products.length;
+//     for (i = noOfResultsPerPage * (pageNo - 1); i < ilimit; i++) {
+//       newProducts[j] = products[i];
+//       j = j + 1;
+//     }
+//     const noPages = Math.ceil(products.length / noOfResultsPerPage);
+//     res
+//       .status(200)
+//       .json({ productList: newProducts, totalNumberOfPages: noPages });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json(error.message);
+//   }
+// };
+
 const getSearchedProducts = async (req, res, next) => {
   try {
     const searchTerm = req.query.q;
-    let pattern = new RegExp(searchTerm, "i");
-    let products = await Product.find({
-      $or: [
-        { name: { $regex: searchTerm, $options: "i" } },
-        { categories: { $in: [pattern] } },
-        { tags: { $in: [pattern] } },
-        { brand: { $in: [pattern] } },
-      ],
-    });
+    let redisOutput = await req.redisClient.get(`search_${searchTerm}`);
 
-    const customerRating = Number(req.query.customerRating);
-    const brands = req.query.brands.split(",");
-    const priceLL = Number(req.query.priceLL);
-    const priceUL = Number(req.query.priceUL);
-    const ruggedVerified = req.query.RuggedVerrified;
-    const coloursSelected = req.query.colours.split(",");
-    const availability = req.query.availability;
-    const pageNo = req.query.pageNo;
-    const noOfResultsPerPage = Number(req.query.noOfResultsPerPage);
-    const categoriesSelected = req.query.categories.split(",");
+    if (redisOutput) {
+      return res.status(200).json(JSON.parse(redisOutput));
+    } else {
+      let pattern = new RegExp(searchTerm, "i");
+      let query = {
+        $or: [
+          { name: { $regex: searchTerm, $options: "i" } },
+          { categories: { $in: [pattern] } },
+          { tags: { $in: [pattern] } },
+          { brand: { $in: [pattern] } },
+        ],
+      };
 
-    const priceFilter = (product) => {
-      let priceAfterDiscount = product.price * (1 - product.discount * 0.01);
-      return priceAfterDiscount > priceLL && priceAfterDiscount < priceUL;
-    };
-
-    const customerRatingFilter = (product) => {
-      // console.log(getAverageRating(product));
-      return getAverageRating(product) >= customerRating;
-    };
-
-    const brandFilter = (product) => {
-      let index = 0;
-      for (index = 0; index < brands.length; index++) {
-        if (product.brand === brands[index]) {
-          return true;
+      // Check if other query parameters exist and modify the query accordingly
+      if (req.query.customerRating && Number(req.query.customerRating) > 0) {
+        query = { ...query, customerRating: { $gte: Number(req.query.customerRating) } };
+      }
+      if (req.query.brands) {
+        const brands = req.query.brands.split(",");
+        if (brands[0] !== "all") {
+          query = { ...query, brand: { $in: brands } };
         }
       }
-      return false;
-    };
-
-    const ruggedVerrifiedFilter = (product) => {
-      return product.ruggedVerrified === true;
-    };
-
-    const colorFilter = (product) => {
-      let index = 0;
-      for (index = 0; index < coloursSelected.length; index++) {
-        let jindex = 0;
-        for (jindex = 0; jindex < product.colours.length; jindex++) {
-          if (coloursSelected[index] === product.colours[jindex]) {
-            return true;
-          }
+      if (req.query.priceLL && req.query.priceUL) {
+        const priceLL = Number(req.query.priceLL);
+        const priceUL = Number(req.query.priceUL);
+        query = { ...query, price: { $gt: priceLL, $lt: priceUL } };
+      }
+      if (req.query.RuggedVerrified === "true") {
+        query = { ...query, ruggedVerrified: true };
+      }
+      if (req.query.colours) {
+        const coloursSelected = req.query.colours.split(",");
+        if (coloursSelected[0] !== "all") {
+          query = { ...query, colours: { $in: coloursSelected } };
         }
       }
-      return false;
-    };
-
-    const categoriesFilter = (product) => {
-      let index = 0;
-      for (index = 0; index < categoriesSelected.length; index++) {
-        let jindex = 0;
-        for (jindex = 0; jindex < product.categories.length; jindex++) {
-          if (categoriesSelected[index] === product.categories[jindex]) {
-            return true;
-          }
+      if (req.query.availability) {
+        query = { ...query, stockQuantity: { $gte: 1 } };
+      }
+      if (req.query.categories) {
+        const categoriesSelected = req.query.categories.split(",");
+        if (categoriesSelected[0] !== "all") {
+          query = { ...query, categories: { $in: categoriesSelected } };
         }
       }
-      return false;
-    };
-    const availabilityFilter = (product) => {
-      return product.stockQuantity >= 1;
-    };
-    // Filter the products based on user's requirements and sort them accordingly
-    if (brands[0] !== "all") {
-      products = products.filter(brandFilter);
-    }
-    if (ruggedVerified === "true") {
-      products = products.filter(ruggedVerrifiedFilter);
-    }
+      console.log(query);
+      let products = await Product.find(query);
+      console.log(products);
+      // Pagination
+      const pageNo = req.query.pageNo ? Number(req.query.pageNo) : 1;
+      const noOfResultsPerPage = req.query.noOfResultsPerPage ? Number(req.query.noOfResultsPerPage) : 10;
+      const startIndex = (pageNo - 1) * noOfResultsPerPage;
+      const endIndex = pageNo * noOfResultsPerPage;
+      const newProducts = products.slice(startIndex, endIndex);
+      const noPages = Math.ceil(products.length / noOfResultsPerPage);
 
-    if (coloursSelected[0] !== "all") {
-      products = products.filter(colorFilter);
-    }
-    if (categoriesSelected[0] !== "all") {
-      products = products.filter(categoriesFilter);
-    }
+      await req.redisClient.set(
+        `search_${searchTerm}`,
+        JSON.stringify({ productList: newProducts, totalNumberOfPages: noPages }),
+        "EX",
+        3600
+      ); // Cache for 1 hour
 
-    if (availability) {
-      products = products.filter(availabilityFilter);
+      return res
+        .status(200)
+        .json({ productList: newProducts, totalNumberOfPages: noPages });
     }
-
-    if (true) {
-      products = products.filter(priceFilter);
-    }
-
-    if (customerRating > 0) {
-      products = products.filter(customerRatingFilter);
-    }
-
-    let newProducts = [];
-    let i = 0;
-    let j = 0;
-    let ilimit =
-      noOfResultsPerPage * pageNo < products.length
-        ? noOfResultsPerPage * pageNo
-        : products.length;
-    for (i = noOfResultsPerPage * (pageNo - 1); i < ilimit; i++) {
-      newProducts[j] = products[i];
-      j = j + 1;
-    }
-    const noPages = Math.ceil(products.length / noOfResultsPerPage);
-    res
-      .status(200)
-      .json({ productList: newProducts, totalNumberOfPages: noPages });
   } catch (error) {
-    console.log(error);
-    res.status(500).json(error.message);
+    res.status(500).json(error);
   }
 };
 
