83c53782818c98ffa32ce7a494341912aa02304b
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2024-05-02 06:30:37 +0530

    cicd works fine

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app=require("./app");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; //port
 
 app.listen(PORT, () => {
   console.log(`Server listeing on PORT:${PORT}`);
